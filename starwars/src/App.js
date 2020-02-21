d1775066a2ebff2a8c670d0b79b8aa38566743b6
CeeDeeBee <cdbarnes98@gmail.com>
2020-02-21 12:08:59 -0500

    Finished MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import PeopleCard from "./components/PeopleCard";
+import { Container, Row } from 'reactstrap';
 import './App.css';
 
 const App = () => {
@@ -8,10 +11,22 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [peopleData, setPeopleData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/")
+      .then(res => setPeopleData(res.data.results))
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Container>
+        <Row>
+          {peopleData.map(person => <PeopleCard props={person} />)}
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ starwars/src/components/PeopleCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import {
+    Card, Button, CardHeader, CardBody,
+    CardText, Col
+} from 'reactstrap';
+import styled from "styled-components";
+
+const StyledCard = styled(Card)`
+    margin: 4% auto;
+`;
+
+const PeopleCard = ({ props }) => {
+    return (
+        <Col sm="12" md="4">
+            <StyledCard>
+                <CardHeader>{props.name}</CardHeader>
+                <CardBody>
+                    <CardText>Gender: {props.gender}</CardText>
+                    <CardText>Birth Year: {props.birth_year}</CardText>
+                    <CardText>Hair Color: {props.hair_color}</CardText>
+                    <CardText>Height: {props.height}</CardText>
+                    <Button>More Info</Button>
+                </CardBody>
+            </StyledCard>
+        </Col>
+    )
+}
+
+export default PeopleCard;
